b282266e7b10c181a50fba976171626e3cae2800
pujith07 <pujithh@pujiths-MacBook-Air.local>
2023-03-09 18:31:25 +0530

    first commit

--- app1/views.py
+++ app1/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def firstapp1(request):
-    return HttpResponse('<h1> function of firstapp1')
+    return HttpResponse('<h1> function of firstapp1</h1>')
 def secondapp1(request):
-    return HttpResponse('<h1> function of secondapp1')
+    return HttpResponse('<h1> function of secondapp1</h1>')
